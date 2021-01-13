5ded453fb8b4f0e0d53ca8bb95e9c09be3d79bf7
Kalyan Adhikari <adhikari.kalyan@gmail.com>
2021-01-13 15:52:41 -0500

    deleting .py file
    deleting .py file

--- OutlierAndMissingDataTreatment.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-%matplotlib inline
-
-df = pd.read_csv('c:/02_Work/workspace/datasets/Lesson3/mycars.csv', delimiter=',')
-df.columns
-
-plt.boxplot(df.hp)
